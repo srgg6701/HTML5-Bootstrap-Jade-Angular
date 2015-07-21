6fc3ef84b5267cbf833d254bbde923a8a8ea8154
srgg6701 <srgg67@gmail.com>
2015-07-21 15:56:29 +0300

    Добавлена текстовый файл, jade-шаблон и т.д..

--- static/js/common.js
+++ static/js/common.js
@@ -1,17 +1,31 @@
 $(function(){
-    var block = 'section',messagesContainer = $('#messages-container');
-    $('.glyphicon', messagesContainer).on('click', function(){
-        var func, currentSection=$(this).parents(block).eq(0);
-        currentSection.slideUp(200, function () {
-            //console.dir([currentSection,currentSection.prev(),currentSection.next()]);
-            if($(currentSection).next(block).length)
-                func='next';
-            else if ($(currentSection).prev(block).length)
-                func='prev';
-            $(currentSection)[func](block)
-                .removeClass('hidden')
-                .slideDown(200);
 
-        });
-    });
+	var mainArea=$('#main-area');
+	if(mainArea.length){
+		syncFooter = function () {
+			var offset = $('footer').outerHeight();
+			mainArea.css({
+				'margin-bottom':'-' + offset + 'px',
+				'padding-bottom': (offset+20) + 'px'
+			});
+		};
+		syncFooter();
+		$(window).on('resize', syncFooter);
+
+	}
+
+	var block = 'section',messagesContainer = $('#messages-container');
+	$('.glyphicon', messagesContainer).on('click', function(){
+		var func, currentSection=$(this).parents(block).eq(0);
+		currentSection.slideUp(200, function () {
+				//console.dir([currentSection,currentSection.prev(),currentSection.next()]);
+				if($(currentSection).next(block).length)
+						func='next';
+				else if ($(currentSection).prev(block).length)
+						func='prev';
+				$(currentSection)[func](block)
+						.removeClass('hidden')
+						.slideDown(200);
+		});
+	});
 });
